9e1d940f78ca8754e584cc7cec283b92126b9791
VANSHCODER009 <gargvansh0003@gmail.com>
2026-02-07 02:51:42 +0530

    Add typing animation logic in script.js
    Implement typing animation for multiple roles.

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Typing Animation Logic */
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'YouTuber', 'Web Designer', 'Programmer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
